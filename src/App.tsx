a97bd794582d205b1aceb2c78263b404a9fd72e4
Posigamerx <git@stackblitz.com>
2025-05-13 17:31:15 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <main>
+   
+     <div className='container'>
+       <img src="../src/avatar.jpeg" alt="Jessica Randall" />
+       <h2>Jessica Randall</h2>
+       <h6>London, United Kingdom</h6>
+       <p>"Front-end web developer and avid reader."</p>
+       <div className='links'>
+            <button>
+            Github
+            </button><button>
+            Frontend Mentor
+            </button><button>
+            LinkedIn
+            </button><button>
+            Twitter
+            </button>
+           <button>
+            Instagram
+            </button>
+       </div>
+     </div>
+    </main>
+  )
+}
